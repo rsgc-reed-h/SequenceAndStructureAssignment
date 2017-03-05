7f8315f07edc9d2afbc38e488f3678d407392682
Student <student@macbook-pro-640-reed.local>
2017-03-05 15:54:44 -0500

    Continued my design

--- SequenceAndStructureAssignment/Assignment.playground/Pages/Design One.xcplaygroundpage/Contents.swift
+++ SequenceAndStructureAssignment/Assignment.playground/Pages/Design One.xcplaygroundpage/Contents.swift
@@ -19,32 +19,47 @@ import Cocoa
 import PlaygroundSupport
 
 // Create canvas
+let offset = random(from: 120, toButNotIncluding: 360)
 let canvas = Canvas(width: 500, height: 500)
-
 // Generate a grid
 canvas.drawShapesWithFill = false
 canvas.defaultBorderWidth = 1
 
-
-for i in stride(from: 25, through: 500, by: 25){
-    for diameter in stride(from: 5, through: 50, by: 10){
-        canvas.drawShapesWithBorders = true
-        canvas.drawShapesWithFill = false
-        canvas.borderColor = Color.black
-        canvas.drawEllipse(centreX: i, centreY: i, width: diameter, height: diameter)
-        canvas.drawEllipse(centreX: 500 - i, centreY: i, width: diameter, height: diameter)
-    }
-}
-
-//for z in stride(from: 475, through: 500, by: 25){
-//    for f in stride(from: 5, through: 50, by: 10){
+//for i in stride(from: 25, through: 500, by: 25){
+//    for diameter in stride(from: 5, through: 50, by: 10){
 //        canvas.drawShapesWithBorders = true
-//        canvas.drawShapesWithFill = false
-//        canvas.borderColor = Color.black
-//        canvas.drawEllipse(centreX: z, centreY: z, width: f, height: f)
+//        canvas.borderColor = Color.init(hue: Float(offset), saturation: 000, brightness: 70, alpha: 50)
+//        canvas.drawEllipse(centreX: i, centreY: i, width: diameter, height: diameter)
+//        canvas.drawEllipse(centreX: 500 - i, centreY: i, width: diameter, height: diameter)
 //    }
 //}
 
+
+
+for x in stride(from: 0, through: 500, by: 75){
+for diameter in stride(from: 0, through: 180, by: 5){
+    canvas.drawShapesWithBorders = true
+    canvas.drawShapesWithFill = false
+    canvas.defaultBorderWidth = 1
+    canvas.borderColor = Color(hue: Float(diameter), saturation: 100, brightness: 100, alpha: 100)
+    canvas.drawEllipse(centreX: x, centreY: x, width: diameter, height: diameter)
+    canvas.drawEllipse(centreX: 500 - x, centreY: x, width: diameter, height: diameter)
+}
+}
+
+
+
+
+
+//for x in stride(from: 0, through: 500, by: 25){
+//for diameter in stride(from: 5, through: 25, by: 1){
+//    for plusone in stride(from: 360, through: 0, by: -1){
+//    canvas.drawShapesWithFill = false
+//    canvas.borderColor = Color(hue: Float(plusone),saturation: 100, brightness: 100, alpha: 100)
+//    canvas.drawEllipse(centreX: x, centreY: 25, width:diameter, height: diameter)
+//    }
+//    }
+//}
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
