a883d66b7c18530d816afa932b1a73a81832f039
Student <student@macbook-pro-640-reed.local>
2017-03-07 11:15:41 -0500

    :)

--- SequenceAndStructureAssignment/Assignment.playground/Pages/Design One.xcplaygroundpage/Contents.swift
+++ SequenceAndStructureAssignment/Assignment.playground/Pages/Design One.xcplaygroundpage/Contents.swift
@@ -19,7 +19,7 @@ import Cocoa
 import PlaygroundSupport
 
 // Create canvas
-let offset = random(from: 120, toButNotIncluding: 360)
+
 let canvas = Canvas(width: 500, height: 500)
 // Generate a grid
 canvas.drawShapesWithFill = false
@@ -35,22 +35,34 @@ canvas.defaultBorderWidth = 1
 //}
 
 
+//
+//for x in stride(from: 0, through: 500, by: 75){
+//for diameter in stride(from: 0, through: 180, by: 5){
+//    canvas.drawShapesWithBorders = true
+//    canvas.drawShapesWithFill = false
+//    canvas.defaultBorderWidth = 1
+//    canvas.borderColor = Color(hue: Float(diameter), saturation: 100, brightness: 100, alpha: 100)
+//    canvas.drawEllipse(centreX: x, centreY: x, width: diameter, height: diameter)
+//    canvas.drawEllipse(centreX: 500 - x, centreY: x, width: diameter, height: diameter)
+//}
+//}
 
-for x in stride(from: 0, through: 500, by: 75){
-for diameter in stride(from: 0, through: 180, by: 5){
-    canvas.drawShapesWithBorders = true
-    canvas.drawShapesWithFill = false
-    canvas.defaultBorderWidth = 1
-    canvas.borderColor = Color(hue: Float(diameter), saturation: 100, brightness: 100, alpha: 100)
-    canvas.drawEllipse(centreX: x, centreY: x, width: diameter, height: diameter)
-    canvas.drawEllipse(centreX: 500 - x, centreY: x, width: diameter, height: diameter)
-}
-}
+canvas.drawText(message: "C", size: 5, x: 450, y: 450)
 
 
-
-
-
+//canvas.drawLine(fromX: 250, fromY: 475, toX: 225, toY: 450)
+//canvas.drawLine(fromX: 225, fromY: 450, toX: 225, toY: 425)
+//canvas.drawLine(fromX: 225, fromY: 425, toX: 250, toY: 400)
+//
+//canvas.drawEllipse(centreX: 25, centreY: 250, width: 40, height: 40)
+//canvas.drawLine(fromX: 250, fromY: 25, toX: 250, toY: 75)
+//canvas.drawLine(fromX: 250, fromY: 75, toX: 275, toY: 50)
+//canvas.drawLine(fromX: 275, fromY: 50, toX: 250, toY: 25)
+//
+//canvas.drawLine(fromX: 450, fromY: 225, toX: 450, toY: 275)
+//canvas.drawLine(fromX: 450, fromY: 225, toX: 475, toY: 225)
+//canvas.drawLine(fromX: 450, fromY: 250, toX: 465, toY: 250)
+//canvas.drawLine(fromX: 450, fromY: 275, toX: 475, toY: 275)
 //for x in stride(from: 0, through: 500, by: 25){
 //for diameter in stride(from: 5, through: 25, by: 1){
 //    for plusone in stride(from: 360, through: 0, by: -1){
