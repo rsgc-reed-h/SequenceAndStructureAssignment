67f79a3622a3625c15ad75233cc76de79246208b
Student <student@macbook-pro-640-reed.local>
2017-03-08 22:42:34 -0500

    I need to add a background colour

--- SequenceAndStructureAssignment/Assignment.playground/Pages/Design One.xcplaygroundpage/Contents.swift
+++ SequenceAndStructureAssignment/Assignment.playground/Pages/Design One.xcplaygroundpage/Contents.swift
@@ -25,53 +25,32 @@ let canvas = Canvas(width: 500, height: 500)
 canvas.drawShapesWithFill = false
 canvas.defaultBorderWidth = 1
 
-//for i in stride(from: 25, through: 500, by: 25){
-//    for diameter in stride(from: 5, through: 50, by: 10){
-//        canvas.drawShapesWithBorders = true
-//        canvas.borderColor = Color.init(hue: Float(offset), saturation: 000, brightness: 70, alpha: 50)
-//        canvas.drawEllipse(centreX: i, centreY: i, width: diameter, height: diameter)
-//        canvas.drawEllipse(centreX: 500 - i, centreY: i, width: diameter, height: diameter)
-//    }
-//}
 
 
-//
-//for x in stride(from: 0, through: 500, by: 75){
-//for diameter in stride(from: 0, through: 180, by: 5){
-//    canvas.drawShapesWithBorders = true
-//    canvas.drawShapesWithFill = false
-//    canvas.defaultBorderWidth = 1
-//    canvas.borderColor = Color(hue: Float(diameter), saturation: 100, brightness: 100, alpha: 100)
-//    canvas.drawEllipse(centreX: x, centreY: x, width: diameter, height: diameter)
-//    canvas.drawEllipse(centreX: 500 - x, centreY: x, width: diameter, height: diameter)
-//}
-//}
+for x in stride(from: 0, through: 500, by: 75){
+for diameter in stride(from: 0, through: 180, by: 5){
+    canvas.drawShapesWithBorders = true
+    canvas.drawShapesWithFill = false
+    canvas.defaultBorderWidth = 1
+    canvas.borderColor = Color(hue: Float(diameter), saturation: 100, brightness: 100, alpha: 100)
+    canvas.drawEllipse(centreX: x, centreY: x, width: diameter, height: diameter)
+    canvas.drawEllipse(centreX: 500 - x, centreY: x, width: diameter, height: diameter)
+}
+}
 
-canvas.drawText(message: "C", size: 5, x: 450, y: 450)
+canvas.drawShapesWithBorders = true
+canvas.drawShapesWithFill = true
+canvas.fillColor = Color.purple
+canvas.drawRectangle(centreX: 258, centreY: 460, width: 50, height: 50, borderWidth: 5)
+canvas.drawRectangle(centreX: 58, centreY: 260, width: 50, height: 50, borderWidth: 5)
+canvas.drawRectangle(centreX: 258, centreY: 60, width: 50, height: 50, borderWidth: 5)
+canvas.drawRectangle(centreX: 458, centreY: 260, width: 50, height: 50, borderWidth: 5)
 
+canvas.drawText(message: "C", size: 20, x: 250, y: 450)
+canvas.drawText(message: "O", size: 20, x: 50, y: 250)
+canvas.drawText(message: "D", size: 20, x: 250, y: 50)
+canvas.drawText(message: "E", size: 20, x: 450, y: 250)
 
-//canvas.drawLine(fromX: 250, fromY: 475, toX: 225, toY: 450)
-//canvas.drawLine(fromX: 225, fromY: 450, toX: 225, toY: 425)
-//canvas.drawLine(fromX: 225, fromY: 425, toX: 250, toY: 400)
-//
-//canvas.drawEllipse(centreX: 25, centreY: 250, width: 40, height: 40)
-//canvas.drawLine(fromX: 250, fromY: 25, toX: 250, toY: 75)
-//canvas.drawLine(fromX: 250, fromY: 75, toX: 275, toY: 50)
-//canvas.drawLine(fromX: 275, fromY: 50, toX: 250, toY: 25)
-//
-//canvas.drawLine(fromX: 450, fromY: 225, toX: 450, toY: 275)
-//canvas.drawLine(fromX: 450, fromY: 225, toX: 475, toY: 225)
-//canvas.drawLine(fromX: 450, fromY: 250, toX: 465, toY: 250)
-//canvas.drawLine(fromX: 450, fromY: 275, toX: 475, toY: 275)
-//for x in stride(from: 0, through: 500, by: 25){
-//for diameter in stride(from: 5, through: 25, by: 1){
-//    for plusone in stride(from: 360, through: 0, by: -1){
-//    canvas.drawShapesWithFill = false
-//    canvas.borderColor = Color(hue: Float(plusone),saturation: 100, brightness: 100, alpha: 100)
-//    canvas.drawEllipse(centreX: x, centreY: 25, width:diameter, height: diameter)
-//    }
-//    }
-//}
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
